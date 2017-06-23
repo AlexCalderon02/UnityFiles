15287c5e6d7888b85e8665b3d7d949388e9949f4
Alex Calderon <alexander.calderon.amc@gmail.com>
2017-06-23 18:56:38 -0500

    Initial Commit

---
+++ GearVR/Gear VR Test/Assets/ToggleVr.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+public class ToggleVr : MonoBehaviour
+{
+    //Example of toggling VRSettings
+    private void Update()
+    {
+        //If V is pressed, toggle VRSettings.enabled
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            VRSettings.enabled = !VRSettings.enabled;
+            Debug.Log("Changed VRSettings.enabled to:" + VRSettings.enabled);
+        }
+    }
+}
---
+++ GearVR/Gear VR Test/Assets/UpdateEyeAnchors.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+public class UpdateEyeAnchors : MonoBehaviour
+{
+    GameObject[] eyes = new GameObject[2];
+    string[] eyeAnchorNames = { "LeftEyeAnchor", "RightEyeAnchor" };
+
+    void Update()
+    {
+        for (int i = 0; i < 2; ++i)
+        {
+            // If the eye anchor is no longer a child of us, don't use it
+            if (eyes[i] != null && eyes[i].transform.parent != transform)
+            {
+                eyes[i] = null;
+            }
+
+            // If we don't have an eye anchor, try to find one or create one
+            if (eyes[i] == null)
+            {
+                Transform t = transform.Find(eyeAnchorNames[i]);
+                if (t)
+                    eyes[i] = t.gameObject;
+
+                if (eyes[i] == null)
+                {
+                    eyes[i] = new GameObject(eyeAnchorNames[i]);
+                    eyes[i].transform.parent = gameObject.transform;
+                }
+            }
+
+            // Update the eye transform
+            eyes[i].transform.localPosition = InputTracking.GetLocalPosition((VRNode)i);
+            eyes[i].transform.localRotation = InputTracking.GetLocalRotation((VRNode)i);
+        }
+    }
+}
